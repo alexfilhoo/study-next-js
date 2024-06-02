1e5c8840a44fd109b5ab854a01e332258e05cf07
Alex Filho <alex.cufars@gmail.com>
2024-06-01 23:41:38 -0300

    feat: adição tag Link e Image do Next

--- primeiroprojeto/src/components/Topo.tsx
+++ primeiroprojeto/src/components/Topo.tsx
@@ -1,14 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Topo() {
   return (
     <>
-      <ul className="flex justify-between items-center bg-zinc-300 h-[60px] p-5">
-        <li>Logo</li>
+      <ul className="flex flex-col justify-between items-center bg-zinc-300 h-auto p-5">
+        <li>
+          <Image
+            src="/AF-logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </li>
         <ul className="flex flex-col">
           <li className="subtitle">Alex Filho</li>
           <li>Desenvolvedor</li>
         </ul>
-        <li>23 anos</li>
+        <ul>
+          <li>
+            <Link href={"/"}>Página Inicial</Link>
+          </li>
+          <li>
+            <Link href={"/produtos/produtos"}>Produtos</Link>
+          </li>
+          <li>
+            <Link href={"/teste/teste"}>Teste</Link>
+          </li>
+        </ul>
       </ul>
+      <div></div>
     </>
   );
 }
